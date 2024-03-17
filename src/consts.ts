34b32b9621f82ad2b7560f629c3a53ae5ace2ce2
Nahum Santana <nahumsantanam@gmail.com>
2024-03-17 14:07:33 -0400

    fix: meta title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nahum Santana - Full Stack Developer';
+export const SITE_DESCRIPTION = "Meet Nahum Santana, a skilled Fullstack developer bringing innovative solutions to life. Explore Nahum's expertise in web development, design, and beyond.";

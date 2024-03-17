38b7256b1570c0fc63cc25af7a19ed250aac0244
Nahum Santana <nahumsantanam@gmail.com>
2024-03-17 11:45:34 -0400

    feat: deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://lordfitoi.github.io',
+	base: 'https://github.com/lordfitoi/lordfitoi.github.io',
 	integrations: [mdx(), sitemap()],
 });

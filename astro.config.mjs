18b62fd210496a06b90522125fecae9baedc2c6b
Nahum Santana <nahumsantanam@gmail.com>
2024-03-17 11:57:23 -0400

    feat: tailwind added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+const site = 'https://lordfitoi.github.io';
+const base = import.meta.env.MODE != "development" ? 'https://lordfitoi.github.io': "";
+
 export default defineConfig({
-	site: 'https://lordfitoi.github.io',
-	base: 'https://github.com/lordfitoi/lordfitoi.github.io',
-	integrations: [mdx(), sitemap()],
+  site: site,
+  base: base,
+  integrations: [mdx(), sitemap(), tailwind()]
 });
